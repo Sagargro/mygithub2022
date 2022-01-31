fcff4798f489dbdbbd6be6ad3d3150af08a4c502
Sagargro <98732566+Sagargro@users.noreply.github.com>
2022-01-31 13:49:12 +0530

    first commit

---
+++ force-app/main/default/lwc/accountsearch1/accountsearch1.js
@@ -0,0 +1,3 @@
+import { LightningElement } from 'lwc';
+
+export default class Accountsearch1 extends LightningElement {}
---
+++ force-app/main/default/lwc/recordIdExample/recordIdExample.js
@@ -0,0 +1,6 @@
+import { LightningElement, wire,api } from 'lwc';
+
+export default class RecordIdExample extends LightningElement {
+
+    @api recordId;
+}
